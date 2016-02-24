78906b154cf57ba5d56d160c3ae42bba8d21d6ed
Ronald Chacon <ronald.chacon87@gmail.com>
2016-02-24 08:36:22 -0800

    user validation specs passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe User, type: :model do
   it { should be_valid }
 
   it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
 end
