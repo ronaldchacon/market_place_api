98be51b53c5acb91b869ea702f82b845426b8170
Ronald Chacon <ronald.chacon87@gmail.com>
2016-02-24 13:34:09 -0800

    Add users#show; tests passing

---
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe Api::V1::UsersController, type: :controller do
+  before(:each) { request.headers['Accept'] = 'application/vnd.example.v1' }
+
+  describe 'GET #show' do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      get :show, id: @user.id, format: :json
+    end
+
+    it "returns the information about a reporter on a hash" do
+      user_response = JSON.parse(response.body, symbolize_names: true)
+      expect(user_response[:email]).to eq @user.email
+    end
+  end
+end
