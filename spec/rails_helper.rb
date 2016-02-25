7a29f08e575bac5e501494e71c3bfcdb9e4f3821
Ronald Chacon <ronald.chacon87@gmail.com>
2016-02-25 13:12:45 -0800

    Add sessions#create action; Tests passing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,7 +56,8 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include Request::JsonHelpers, type: :controller
-  config.include Request::HeadersHelpers, :type => :controller
+  config.include Request::HeadersHelpers, type: :controller
+  config.include Devise::TestHelpers, type: :controller
   config.before(:each, type: :controller) do
     include_default_accept_headers
   end
