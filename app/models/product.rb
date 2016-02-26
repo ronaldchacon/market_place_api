0a9d430e64f6588bc0cca3bc9217f400b906c4f3
Ronald Chacon <ronald.chacon87@gmail.com>
2016-02-26 14:57:01 -0800

    Add search scopes on Product model; Tests passing

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,20 @@ class Product < ActiveRecord::Base
   validates :title, :user_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 },
                     presence: true
+
+  def self.filter_by_title(keyword)
+    where('lower(title) LIKE :keyword', keyword: "%#{keyword.downcase}%")
+  end
+
+  def self.above_or_equal_to_price(price)
+    where('price >= :price', price: price)
+  end
+
+  def self.below_or_equal_to_price(price)
+    where('price <= :price', price: price)
+  end
+
+  def self.recent
+    order('updated_at')
+  end
 end
