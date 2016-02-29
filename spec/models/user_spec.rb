6d1b2a6aac6b1a866508a0dcfa2afb9ca0c1d945
Ronald Chacon <ronald.chacon87@gmail.com>
2016-02-29 15:47:43 -0800

    Add user/order has_many

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,16 +17,17 @@ RSpec.describe User, type: :model do
   it { should validate_uniqueness_of(:auth_token) }
 
   it { should have_many(:products) }
+  it { should have_many(:orders)}
 
   describe '#generate_authentication_token!' do
-    it 'generates a unique token' do
-      Devise.stub(:friendly_token).and_return('auniquetoken123')
+    it "generates a unique token" do
+      Devise.stub(:friendly_token).and_return("auniquetoken123")
       @user.generate_authentication_token!
-      expect(@user.auth_token).to eq 'auniquetoken123'
+      expect(@user.auth_token).to eq "auniquetoken123"
     end
 
-    it 'generates another token when one already has been taken' do
-      existing_user = FactoryGirl.create(:user, auth_token: 'auniquetoken123')
+    it "generates another token when one already has been taken" do
+      existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
       @user.generate_authentication_token!
       expect(@user.auth_token).not_to eq existing_user.auth_token
     end
@@ -38,7 +39,7 @@ RSpec.describe User, type: :model do
       3.times { FactoryGirl.create :product, user: @user }
     end
 
-    it 'destroys the associated products on self destruct' do
+    it "destroys the associated products on self destruct" do
       products = @user.products
       @user.destroy
       products.each do |product|
