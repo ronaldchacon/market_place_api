1a2200bc6c7de60fdbbd35bd86666edbb0b6da7a
Ronald Chacon <ronald.chacon87@gmail.com>
2016-03-01 12:51:38 -0800

    Add quantity to placements

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,7 +15,7 @@ class Order < ActiveRecord::Base
     product_ids_and_quantities.each do |product_id_and_quantity|
       id, quantity = product_id_and_quantity
 
-      placements.new(product_id: id)
+      placements.new(product_id: id, quantity: quantity)
     end
   end
 end
