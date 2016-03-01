4a06ab3047fd5f9c5f4a2833ac0731365b3b6280
Ronald Chacon <ronald.chacon87@gmail.com>
2016-02-29 18:25:31 -0800

    Add orders#create action. Tests passing

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -30,4 +30,22 @@ RSpec.describe Api::V1::OrdersController, type: :controller do
 
     it { should respond_with 200 }
   end
+
+  describe "POST  #create" do
+    before(:each) do
+      current_user = FactoryGirl.create(:user)
+      api_authorization_header(current_user.auth_token)
+      product_1 = FactoryGirl.create(:product)
+      product_2 = FactoryGirl.create(:product)
+      order_params = { product_ids: [product_1.id, product_2.id] }
+      post :create, user_id: current_user.id, order: order_params
+    end
+
+    it "returns the just created order by the user" do
+      order_response = json_response
+      expect(order_response[:id]).to be_present
+    end
+
+    it { should respond_with 201 }
+  end
 end
